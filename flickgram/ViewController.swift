650f542f79370a0cb9658a9b9ba9dfcbdd0775a9
Bhavesh Shah <bhaveshshah@bhaveshs-macbook-air.local>
2018-10-22 12:45:25 -0400

    Initial Commit

---
+++ flickgram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flickgram
+//
+//  Created by Bhavesh Shah on 10/22/18.
+//  Copyright © 2018 Bhavesh Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
